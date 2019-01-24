27415c591df9e3b86b4931364bb232ccb2af8e4b
Denica Nedjalkova <2298079n@student.gla.ac.uk>
2019-01-24 00:53:43 +0000

    Creating and updating Models/Tables

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+
+	def __str__(self):
+	# generate string representation of the class
+		return self.name
+
+
+class Page(models.Model):
+	# Category is specified as an argument to the field's constructor
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
