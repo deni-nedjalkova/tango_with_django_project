de8040e2d5261018a02c821789fcff29266d6fe5
Denica Nedjalkova <2298079n@student.gla.ac.uk>
2019-01-24 21:01:54 +0000

    End of chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, Question, Choice
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # numberof slots
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
