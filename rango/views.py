bd922aa51fc99427fe9e0eaae805c4ecba7c2495
Denica Nedjalkova <2298079n@student.gla.ac.uk>
2019-02-08 16:18:17 +0000

    End of chapter 10

--- rango/views.py
+++ rango/views.py
@@ -10,12 +10,45 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # a view called index
 # each view takes at least one argument - HttpRequest obj
 # returns HttpResponse object - takes string as a parameter representing
 # the content of the page to be sent to the client
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	# Get the number of visits to the site.
+	# We use the COOKIES.get() function to obtain the visits cookie.
+	# If the cookie exists, the value returned is casted to an integer.
+	# If the cookie doesn't exist, then the default value of 1 is used.
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+												'last_visit',
+												str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+												'%Y-%m-%d %H:%M:%S')
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		#update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+	
+	# Update/set the visits cookie
+	request.session['visits'] = visits
+
 def index(request):
+	request.session.set_test_cookie()
 	# return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
 	# context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 	context_dict = {}
@@ -24,11 +57,29 @@ def index(request):
 	context_dict['categories'] = category_list
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict['pages'] = page_list
-	return render(request, 'rango/index.html', context=context_dict)
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
+	response = render(request, 'rango/index.html', context=context_dict)
+
+	return response
 
 def about(request):
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
+
+	context_dict = {}
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
 	#return HttpResponse("Rango says here is the about page. <br/><a href='/rango/'>Index</a>.")
-	return render(request, 'rango/about.html', {})
+	
+	response = render(request, 'rango/about.html', context=context_dict)
+
+	return response
 
 def show_category(request,category_name_slug):
 	# Create a context dictionary which we can pass
@@ -56,6 +107,7 @@ def add_category(request):
 		if form.is_valid():
 			# Save the new category to the database.
 			form.save(commit=True)
+			print(category, category.slug)
 			# Recently category added is on the index page --
 			# Direct the user back to the index page
 			return index(request)
